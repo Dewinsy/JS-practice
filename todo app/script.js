b5d350447a29315b89a2969b066d3c2a4c6016f7
Dewinsy <mushdaisy71@gmail.com>
2021-04-27 12:24:10 +0300

    new changes

--- todo app/script.js
+++ todo app/script.js
@@ -1,22 +1,61 @@
+// Referenciing all the necessary Elements.
+
 let ourInput = document.querySelector(".our-input");
-let ourBtn = document.querySelector(".our-btn");
-let ourContainer= document.querySelector(".our-container");
+let ourTodoList = document.querySelector(".our-todolist");
 
+// console.log(ourInput, ourBtn, ourTodoList);
 
-console.log(`Input Element ${ourInput}`);
-console.log(`Button Element ${ourBtn}`);
-console.log(`Container Element ${ourContainer}`);
-
-ourContainer.style.border = "1px solid black";
-ourContainer.style.height= "100px";
-ourContainer.innerHTML = "<h2>hello guys...</h2>";
-
-function change(){
+function AddTodo() {
+  if (ourInput.value.length > 0) {
     console.log(ourInput.value);
-    ourContainer.innerHTML = `<li> ${ourInput}.value </li>`
-    
+    // Creating our 'li' dynamically using Javascript.
+    let ourLi = document.createElement("li");
+    // set the innetHTML to the input value.
+
+    //set attribute
+  ourLi.setAttribute("class","list-group-item list-group-item-action");
+  //ourLi.style.backgroundColor=r
+ // ourLi.style.backgroundColor= "#f4f0ec";
+
+    // concatenating the value to an html button
+    // ourLi.innerHTML = ourInput.value+"<button> delete</button>"
+    ourLi.innerHTML = `${ourInput.value}<button class='del-btn btn position-absolute bottom-0 end-0'> <i class="fa fa-trash danger"> </button>`;
+
+    // This adds the li from line 13 to the ol which is in our html
+    ourTodoList.appendChild(ourLi);
+
+    ourInput.value = "";
+    RemoveTodo();
+    FinishTodo()
+  } else {
+    alert("Please type something");
+  }
 }
 
+ourInput.addEventListener("change", AddTodo);
+
+// Remove Todo
+function RemoveTodo() {
+  let removeBtns = document.querySelectorAll(".del-btn");
+  //removeBtns.setAttribute("class","btn me-auto")
+  removeBtns.forEach(function (button) {
+    button.addEventListener("click", function () {
+      this.parentElement.remove();
+    });
+  });
+}
+
+// Finish Todo
+function FinishTodo(){
+    let ourtodos = document.querySelectorAll("li")
+    ourtodos.forEach(function(li){
+        li.addEventListener("click",function(){
+          li.classList.toggle("finished")
+          
+        })
+    })
+
+}
 
 
 
